e8a24882462a2afd154fd039d0416c2aa0bb5d85
brighter <brighter0611@gmail.com>
2024-07-09 12:08:37 -0700

    second push

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -45,13 +45,27 @@ function PageHome() {
     <div className="nc-PageHome relative overflow-hidden">
       <div>
         <ul className="flex justify-center gap-8 py-4">
-          <li className="hover:text-green-400"><a href="/epic-games">Epic Games</a></li>
-          <li className="hover:text-green-400"><a href="/aluguel-de-contas">Jogos Online PC</a></li>
-          <li className="hover:text-green-400"><a href="/microsoft-store">Microsoft Store</a></li>
-          <li className="hover:text-green-400"><a href="/origin">Origin</a></li>
-          <li className="hover:text-green-400"><a href="/pre-Venda">Pre-Venda</a></li>
-          <li className="hover:text-green-400"><a href="/steam">Steam</a></li>
-          <li className="hover:text-green-400"><a href="/uplay">Ubsoft</a></li>
+          <li className="hover:text-green-400">
+            <a href="/epic-games">Epic Games</a>
+          </li>
+          <li className="hover:text-green-400">
+            <a href="/aluguel-de-contas">Jogos Online PC</a>
+          </li>
+          <li className="hover:text-green-400">
+            <a href="/microsoft-store">Microsoft Store</a>
+          </li>
+          <li className="hover:text-green-400">
+            <a href="/origin">Origin</a>
+          </li>
+          <li className="hover:text-green-400">
+            <a href="/pre-Venda">Pre-Venda</a>
+          </li>
+          <li className="hover:text-green-400">
+            <a href="/steam">Steam</a>
+          </li>
+          <li className="hover:text-green-400">
+            <a href="/uplay">Ubsoft</a>
+          </li>
         </ul>
       </div>
       <SectionHero2 />
