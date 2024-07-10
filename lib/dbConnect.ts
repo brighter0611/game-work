7ef635a585003634dc2d1089bcfa2ea3ca9c235e
brighter <brighter0611@gmail.com>
2024-07-10 15:57:03 -0700

    Last project

--- lib/dbConnect.ts
+++ lib/dbConnect.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const dbConnect = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI!);
+    await mongoose.connect(process.env.NEXT_PUBLIC_MONGO_URI!);
   } catch (error) {
     throw new Error('Connection Failed!');
   }
